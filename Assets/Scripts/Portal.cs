f268e8f81ad2b6295a488e1cc027a142d37d9431
Zo0MER <exzo0mex@gmail.com>
2014-11-14 10:09:42 +0300

    Project copied from MathManiac

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+
+    static private int currentLvl = 0;
+    private float angleSpeed = 2.0f;
+    private float pause = 2.0f;
+    private bool teleport;
+    
+	// Use this for initialization
+	void Start () {
+        ((GameObject)GameObject.FindGameObjectWithTag("WinText")).GetComponent<TextMesh>().renderer.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    transform.Rotate(new Vector3(0, 0, angleSpeed));
+        
+        if(teleport)
+            pause -= Time.deltaTime;
+        if (pause < 0)
+        {
+            Application.LoadLevel(++currentLvl % Application.levelCount);
+
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        teleport = true;
+        ((GameObject)GameObject.FindGameObjectWithTag("WinText")).GetComponent<TextMesh>().renderer.enabled = true;
+          
+    }
+}
