94ba0ecb5e2acb0f6937324ae7110660dcfe5dda
Zo0MER <exzo0mex@gmail.com>
2015-05-26 21:44:14 +0400

    Collider for graph replaced with edge collider

--- Assets/Scripts/LineGenerator/GraphicLineGenerator.cs
+++ Assets/Scripts/LineGenerator/GraphicLineGenerator.cs
@@ -41,23 +41,6 @@ public class GraphicLineGenerator : MonoBehaviour {
             lineRenderer.SetPosition(i, points[i]);
         }
 
-        List<Vector2> lowerLine = new List<Vector2>();
-        Vector2 vec = new Vector2(0, 0);
-        for (int i = 0; i < points.Count - 1; i++)
-        {
-            vec = points[i] - points[i + 1];
-            vec.Normalize();
-            vec = Quaternion.AngleAxis(90, Vector3.forward) * vec;
-            lowerLine.Add(points[i] + new Vector2(vec.x * bold, vec.y * bold));
-        }
-        lowerLine.Add(points[points.Count - 1] + new Vector2(vec.x * bold, vec.y * bold));
-        lowerLine.Reverse();
-        points.AddRange(lowerLine);
-
-
-
-        this.GetComponent<PolygonCollider2D>().SetPath(0, points.ToArray());
-
-        
+        GetComponent<EdgeCollider2D>().points = points.ToArray();
     }
 }
