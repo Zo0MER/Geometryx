ad7eca5f034f0918f3e41b223274c93376a77d0e
Zo0MER <exzo0mex@gmail.com>
2016-01-21 18:47:40 +0300

    Added main menu

---
+++ Assets/LevelMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelMenu : MonoBehaviour {
+
+    public void OpenLevel(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
---
+++ Assets/MenuPanel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuPanel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        
+	    foreach (Transform child in transform)
+	    {
+	        child.transform.localScale = Vector3.zero;
+	        LeanTween.scale(child.gameObject, Vector3.one, Random.Range(0.4f, 0.8f)).setEase(LeanTweenType.easeOutCubic);
+	    }
+	}
+	
+	
+}
